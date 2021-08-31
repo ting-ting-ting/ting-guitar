35311d3a18c12b1231dcfb693902fce76c90a9f6
TingTing <ting@cakeresume.com>
2021-08-31 17:08:04 +0800

    create number const

--- src/tabs/Mayday/Angel.jsx
+++ src/tabs/Mayday/Angel.jsx
@@ -5,7 +5,53 @@ import Line from '../../components/Line';
 const Angel = () => {
   return (
     <>
-      <Line />
+      <Line
+        line={{
+          chords: [
+            [
+              {
+                number: '1',
+                type: 'maj',
+              },
+              null,
+              null,
+              null,
+            ],
+            [
+              {
+                number: '6',
+                type: 'm',
+              },
+              null,
+              null,
+              null,
+            ],
+            [
+              {
+                number: '4',
+                type: 'maj',
+              },
+              null,
+              null,
+              null,
+            ],
+            [
+              {
+                number: '5',
+                type: 'maj',
+              },
+              null,
+              null,
+              null,
+            ],
+          ],
+          words: [
+            '你就是我的天使',
+            '保護著我的天使',
+            '從此我再也沒有憂傷',
+          ],
+        }}
+      />
     </>
   );
 };
