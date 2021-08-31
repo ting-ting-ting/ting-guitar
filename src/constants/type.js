df39693c26f42bf0d5108faac99c5cdef5f512e5
TingTing <ting@cakeresume.com>
2021-08-31 17:11:30 +0800

    create type const

---
+++ src/constants/type.js
@@ -0,0 +1,8 @@
+// Major
+export const TYPE_MAJOR = 'TYPE_MAJOR';
+export const TYPE_MAJOR_SEVEN = 'TYPE_MAJOR_SEVEN';
+
+
+// Minor
+export const TYPE_MINOR = 'TYPE_MINOR';
+export const TYPE_MINOR_SEVEN = 'TYPE_MINOR_SEVEN';
