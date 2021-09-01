c427e5558378e004a61d2d681c8245c46ca912e2
TingTing <ting@cakeresume.com>
2021-09-01 17:59:58 +0800

    angel

--- src/constants/tabs/mayday/angel.js
+++ src/constants/tabs/mayday/angel.js
@@ -1,5 +1,6 @@
 import {
   NUMBER_ONE,
+  NUMBER_THREE,
   NUMBER_FOUR,
   NUMBER_FIVE,
   NUMBER_SIX,
@@ -106,7 +107,100 @@ const angelTabData = {
         ],
       ],
       lyrics: ['▲你就是我的天使 保護著我的天使 從此我再也沒有憂傷'],
-    }
+    },
+    {
+      measures: [
+        [
+          {
+            number: NUMBER_ONE,
+            type: TYPE_MAJOR,
+          },
+          null,
+          null,
+          null,
+        ],
+        [
+          {
+            number: NUMBER_SIX,
+            type: TYPE_MINOR,
+          },
+          null,
+          null,
+          null,
+        ],
+        [
+          {
+            number: NUMBER_FOUR,
+            type: TYPE_MAJOR,
+          },
+          null,
+          null,
+          null,
+        ],
+        [
+          {
+            number: NUMBER_FIVE,
+            type: TYPE_MAJOR,
+          },
+          null,
+          null,
+          null,
+        ],
+      ],
+      lyrics: ['你就是我的天使 給我快樂的天使 甚至我學會了飛翔'],
+    },
+    {
+      measures: [
+        [
+          {
+            number: NUMBER_SIX,
+            type: TYPE_MINOR,
+          },
+          null,
+          {
+            number: NUMBER_THREE,
+            type: TYPE_MINOR,
+          },
+          null,
+        ],
+        [
+          {
+            number: NUMBER_FOUR,
+            type: TYPE_MAJOR,
+          },
+          null,
+          {
+            number: NUMBER_ONE,
+            type: TYPE_MAJOR,
+          },
+          null,
+        ],
+      ],
+      lyrics: ['飛過人間的無常 才懂愛才是寶藏'],
+    },
+    {
+      measures: [
+        [
+          {
+            number: NUMBER_FIVE,
+            type: TYPE_MAJOR,
+          },
+          null,
+          null,
+          null,
+        ],
+        [
+          {
+            number: NUMBER_FOUR,
+            type: TYPE_MAJOR,
+          },
+          null,
+          null,
+          null,
+        ],
+      ],
+      lyrics: ['不管世界變得怎麼樣 只要有你就會是天堂'],
+    },
   ],
 };
 
