19c8e4392d6cd44069a14ed50d918ded5354cabe
TingTing <ting@cakeresume.com>
2021-09-01 10:29:37 +0800

    create note name constants

--- src/constants/type.js
+++ src/constants/type.js
@@ -1,8 +1,8 @@
 // Major
-export const TYPE_MAJOR = 'TYPE_MAJOR';
-export const TYPE_MAJOR_SEVEN = 'TYPE_MAJOR_SEVEN';
+export const TYPE_MAJOR = 'TYPE/TYPE_MAJOR';
+export const TYPE_MAJOR_SEVEN = 'TYPE/TYPE_MAJOR_SEVEN';
 
 
 // Minor
-export const TYPE_MINOR = 'TYPE_MINOR';
-export const TYPE_MINOR_SEVEN = 'TYPE_MINOR_SEVEN';
+export const TYPE_MINOR = 'TYPE/TYPE_MINOR';
+export const TYPE_MINOR_SEVEN = 'TYPE/TYPE_MINOR_SEVEN';
