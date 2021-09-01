f1da34ad6d1224b857afa3c6ea5ffec26790d197
TingTing <ting@cakeresume.com>
2021-09-01 10:25:27 +0800

    create TonalitySelector component

---
+++ src/components/TonalitySelector/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.scss';
+
+const TonalitySelector = ({
+
+}) => {
+  return (
+    <div className="tonality-selector">
+      TonalitySelector
+    </div>
+  );
+};
+
+export default React.memo(TonalitySelector);
