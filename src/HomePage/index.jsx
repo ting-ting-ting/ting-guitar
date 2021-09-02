af9f09c85181bdb72043bb3c5ae1c13ae88950cc
TingTing <ting@cakeresume.com>
2021-09-02 14:45:58 +0800

    group tabs by singer

--- src/HomePage/index.jsx
+++ src/HomePage/index.jsx
@@ -2,20 +2,27 @@ import React from 'react';
 import {
   Link,
 } from "react-router-dom";
+import { groupBy } from 'lodash';
 import { tablist, tabData } from '../constants/tabs';
 
-const HomePage = () => (
-  <div>
-    <nav>
-      <ul>
-        {tablist.map(id => (
-          <li key={id}>
-            <Link to={`/tab/${id}`}>{tabData[id].name}</Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  </div>
-);
+const HomePage = () => {
+  const tabsAfterGroup = groupBy(tablist.map(id => tabData[id]), 'singer');
+
+  console.log('tabs', tabsAfterGroup)
+
+  return (
+    <div>
+      <nav>
+        <ul>
+          {tablist.map(id => (
+            <li key={id}>
+              <Link to={`/tab/${id}`}>{tabData[id].name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
 export default React.memo(HomePage);
