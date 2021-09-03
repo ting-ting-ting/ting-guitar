322a5d081f4f11d8c98c2d362b359b831d96c8b6
TingTing <ting@cakeresume.com>
2021-09-03 17:33:03 +0800

    add sus types

--- src/constants/type.js
+++ src/constants/type.js
@@ -1,8 +1,12 @@
-// Major
+// major
 export const TYPE_MAJOR = 'TYPE/TYPE_MAJOR';
 export const TYPE_MAJOR_SEVEN = 'TYPE/TYPE_MAJOR_SEVEN';
 
 
-// Minor
+// minor
 export const TYPE_MINOR = 'TYPE/TYPE_MINOR';
 export const TYPE_MINOR_SEVEN = 'TYPE/TYPE_MINOR_SEVEN';
+
+// sus
+export const TYPE_SUS2 = 'TYPE/TYPE_SUS2';
+export const TYPE_SUS4 = 'TYPE/TYPE_SUS4';
