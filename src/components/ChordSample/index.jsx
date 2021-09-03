fc2a948ebc06caee35d2322ebc5a22c569bd2c9b
TingTing <ting@cakeresume.com>
2021-09-03 16:19:34 +0800

    add chord sample component

---
+++ src/components/ChordSample/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.scss';
+
+const ChordSample = ({
+
+}) => {
+  return (
+    <div className="chord-sample">
+      ChordSample
+    </div>
+  );
+};
+
+export default React.memo(ChordSample);
