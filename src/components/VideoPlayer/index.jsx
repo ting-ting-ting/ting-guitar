329825b3a9581bcffac73e45186b67bd9b3371e0
TingTing <ting@cakeresume.com>
2021-09-03 10:51:26 +0800

    refactor video component coding

---
+++ src/components/VideoPlayer/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactPlayer from 'react-player/lazy';
+import './index.scss';
+
+const VideoPlayer = ({
+  url,
+}) => (
+  <div className="player-wrapper">
+    <ReactPlayer
+      className='react-player'
+      url={url}
+      width="100%"
+      height="100%"
+    />
+  </div>
+);
+
+export default React.memo(VideoPlayer);
