8cdef3495d62881d296fc34b9a8d02260348d590
TingTing <ting@cakeresume.com>
2021-09-08 18:00:45 +0800

    add pia song

--- src/constants/tabs/pia/index.js
+++ src/constants/tabs/pia/index.js
@@ -1,5 +1,16 @@
-const pia = {
+import newsletterSong from './newsletterSong';
 
+const singerInfoData = {
+  singer: 'PiA',
+  singerId: 'pia',
 };
+
+const pia = {
+  'pia-1': {
+    id: 'pia-1',
+    ...singerInfoData,
+    ...newsletterSong,
+  },
+}
 
 export default pia;
