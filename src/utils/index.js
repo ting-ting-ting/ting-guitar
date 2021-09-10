4ab513890ca9d7f4b50aef071fafd23881406a87
TingTing <ting@cakeresume.com>
2021-09-10 14:30:12 +0800

    more number mapping

--- src/utils/index.js
+++ src/utils/index.js
@@ -1296,8 +1296,20 @@ export function numberTransfer({
         case NOTE_NAME_C:
           return 'bA';
 
+        case NOTE_NAME_C_SHARP:
+          return 'A';
+
+        case NOTE_NAME_D_FLAT:
+          return 'A';
+
         case NOTE_NAME_D:
           return 'bB';
+
+        case NOTE_NAME_D_SHARP:
+          return 'B';
+
+        case NOTE_NAME_E_FLAT:
+          return 'B';
 
         case NOTE_NAME_E:
           return 'C';
@@ -1305,44 +1317,32 @@ export function numberTransfer({
         case NOTE_NAME_F:
           return 'bD';
 
+        case NOTE_NAME_F_SHARP:
+          return 'D';
+
+        case NOTE_NAME_G_FLAT:
+          return 'D';
+
         case NOTE_NAME_G:
           return 'bE';
+
+        case NOTE_NAME_G_SHARP:
+          return 'E';
+
+        case NOTE_NAME_A_FLAT:
+          return 'E';
 
         case NOTE_NAME_A:
           return 'F';
 
-        case NOTE_NAME_B:
-          return 'G';
-
-        case NOTE_NAME_C_SHARP:
-          return '';
-
-        case NOTE_NAME_D_FLAT:
-          return '';
-
-        case NOTE_NAME_D_SHARP:
-          return '';
-
-        case NOTE_NAME_E_FLAT:
-          return '';
-
-        case NOTE_NAME_F_SHARP:
-          return '';
-
-        case NOTE_NAME_G_FLAT:
-          return '';
-
-        case NOTE_NAME_G_SHARP:
-          return '';
-
-        case NOTE_NAME_A_FLAT:
-          return '';
-
         case NOTE_NAME_A_SHARP:
-          return '';
+          return '#F';
 
         case NOTE_NAME_B_FLAT:
-          return '';
+          return 'bG';
+
+        case NOTE_NAME_B:
+          return 'G';
 
         default:
           return 'b6';
