8f1d110c4e2680307ce2e68560e118498c0ba4c6
TingTing <ting@cakeresume.com>
2021-09-10 14:54:20 +0800

    add song

---
+++ src/constants/tabs/movie/southOfCountry.js
@@ -0,0 +1,33 @@
+import {
+  NUMBER_ONE,
+  NUMBER_TWO,
+  NUMBER_THREE,
+  NUMBER_FOUR,
+  NUMBER_FIVE,
+  NUMBER_SIX,
+  NUMBER_SEVEN,
+} from '../../number';
+import {
+  TYPE_MAJOR,
+  TYPE_MAJOR_ADD9,
+  TYPE_MINOR,
+  TYPE_MINOR7,
+  TYPE_MINOR7_ADD11,
+} from '../../type';
+import {
+  NOTE_NAME_C,
+} from '../../noteName';
+
+const southOfCountry = {
+  name: '國境之南',
+  originKey: NOTE_NAME_C,
+  play: NOTE_NAME_C,
+  capo: 0,
+  video: '',
+  samples: [],
+  lines: [
+
+  ],
+};
+
+export default southOfCountry;
